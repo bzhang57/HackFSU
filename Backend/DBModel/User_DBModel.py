3f179e9317a40a508c7897c1366a81ce0fb8c611
Bryan <bzhang57@gatech.edu>
2018-03-03 18:59:03 -0500

    backend stuff

--- Backend/DBModel/User_DBModel.py
+++ Backend/DBModel/User_DBModel.py
@@ -1,11 +1,8 @@
 import json
 import uuid
-from Utility.Encryptor import Encryptor
-from Utility.color_print import ColorPrint
 from flaskapp import db, app
 from flask import request, Response, send_file, send_from_directory, make_response, session
 from sqlalchemy.dialects.postgresql import JSON
-from DBModel.User_DBModel import User_DBModel
 import datetime
 
 class User_DBModel(db.Model):
@@ -13,31 +10,29 @@ class User_DBModel(db.Model):
 	id = db.Column(db.Text, primary_key = True)
 	password = db.Column(db.Text, primary_key = False)
 	email = db.Column(db.Text)
-    name = db.Column(db.Text)
-    password = db.Column(db.Text)
-    tags = db.Column(db.Text)
-    following = db.Column(db.Text)
-    followers = db.Column(db.Text)
-    saved_recipes = db.Column(db.Text)
+	name = db.Column(db.Text)
+	password = db.Column(db.Text)
+	following = db.Column(db.Text)
+	followers = db.Column(db.Text)
+	saved_recipes = db.Column(db.Text)
 
-	def __init__(self, id, name, password, email, tags, following, followers, saved_recipes):
-        self.id = id
-        self.name = name
-        self.password = password
-        self.email = email
-        self.tags = tags
-        self.following = following
-        self.followers = followers
-        self.saved_recipes = saved_recipes
+	def __init__(self, id, name, password, email, following, followers, saved_recipes):
+		self.id = id
+		self.name = name
+		self.password = password
+		self.email = email
+		self.following = following
+		self.followers = followers
+		self.saved_recipes = saved_recipes
 
-    @staticmethod
-    def authenticate_email_password(email, password):
-        if not email or not password:
-            return False
-        user_info = User_DBModel.query.filter_by(email = email).first()
-        if user_info is None:
-                return None
-        if user_info.password is not None:
-                if user_info.password == password:
-                        return user_info
-        return None
+	@staticmethod
+	def authenticate_email_password(email, password):
+		if not email or not password:
+			return False
+		user_info = User_DBModel.query.filter_by(email = email).first()
+		if user_info is None:
+				return None
+		if user_info.password is not None:
+				if user_info.password == password:
+						return user_info
+		return None
--- Backend/Model/User.py
+++ Backend/Model/User.py
@@ -1,16 +1,11 @@
 import json
 import uuid
-from Utility.Encryptor import Encryptor
-from Utility.color_print import ColorPrint
 from flaskapp import db, app
 from flask import request, Response, send_file, send_from_directory, make_response, session
 from sqlalchemy.dialects.postgresql import JSON
 from DBModel.User_DBModel import User_DBModel
 import datetime
 
-encryptor = Encryptor()
-seconds_in_hour = 60*60
-session_inactivity_timeout = datetime.timedelta(0, seconds_in_hour * 2)
 
 class User():
 
@@ -71,8 +66,9 @@ class User():
 				dict_local = {'id': report.id,
 												'name': report.name,
 												'email': report.email,
-												'password' : report.password
+												'password' : report.password,
 												'tags': report.tags
+												}
 
 				return_json_list.append(dict_local)
 			return_string = json.dumps(return_json_list, sort_keys=True, indent=4, separators=(',', ': '))
@@ -89,11 +85,11 @@ class User():
 		email = parsed_json["email"]
 		password = parsed_json["password"]
 		name = parsed_json["name"]
-		tags = parset_json["tags"]
+	
 
 		if User_DBModel.query.filter_by(email = email).first() is None:
 			id = str(uuid.uuid4())
-			user = User_DBModel(id, name, password, email, tags, [], [])
+			user = User_DBModel(id, name, password, email, None, None, None)
 			db.session.add(user)
 			db.session.commit()
 
@@ -116,73 +112,59 @@ class User():
 		return_string = json.dumps(return_dict, sort_keys=True, indent=4, seperators=(',',': '))
 		return return_string
 
-	def add_user_tags():
-		if 'user' in session.keys():
-			user = session['user']
-			parsed_json = request.get_json()
-			user_info = User_DBModel.query.filter_by(id = user["id"]).first()
-			user_info.tags.append(parsed_json['added_tag'])
-
-			db.session.commit()
-			return_dict = {'code': 200}
-			return_string = json.dumps(return_dict, sort_keys=True, indent=4, seperators=(',',': '))
-			return return_string
-		else:
-			dict_local = {'code': 31, 'message': "auth error"}
-			return_string = json.dumps(dict_local, sort_keys=True, indent=4, separators=(',', ': '))
-			return return_string
-
 	def add_user_follower():
-		if 'user' in session.keys():
-			user = session['user']
-			parsed_json = request.get_json()
-			user_info = User_DBModel.query.filter_by(id = user["id"]).first()
-			user_info.followers.append(parsed_json['added_followers'])
-
-			db.session.commit()
-			return_dict = {'code': 200}
-			return_string = json.dumps(return_dict, sort_keys=True, indent=4, seperators=(',',': '))
-			return return_string
-		else:
-			dict_local = {'code': 31, 'message': "auth error"}
-			return_string = json.dumps(dict_local, sort_keys=True, indent=4, separators=(',', ': '))
-			return return_string
-
-	def add_user_following():
-	if 'user' in session.keys():
-		user = session['user']
 		parsed_json = request.get_json()
-		user_info = User_DBModel.query.filter_by(id = user["id"]).first()
-		user_info.following.append(parsed_json['added_following'])
-
+		user_info = User_DBModel.query.filter_by(id = parsed_json["id"]).first()
+		if (user_info.followers == None):
+			user_info.followers = parsed_json['added_followers']
+		else:
+			if (parsed_json['added_followers'] not in user_info.followers):
+				user_info.followers += ";" + parsed_json['added_followers']
+		db.session.add(user_info)
 		db.session.commit()
 		return_dict = {'code': 200}
-		return_string = json.dumps(return_dict, sort_keys=True, indent=4, seperators=(',',': '))
+		return_string = json.dumps(return_dict, sort_keys=True, indent=4, separators=(',',':'))
 		return return_string
-	else:
-		dict_local = {'code': 31, 'message': "auth error"}
-		return_string = json.dumps(dict_local, sort_keys=True, indent=4, separators=(',', ': '))
+	
+
+	def add_user_following():
+	
+
+		parsed_json = request.get_json()
+		user_info = User_DBModel.query.filter_by(id = parsed_json["id"]).first()
+		if (user_info.following == None):
+			user_info.following = parsed_json["added_following"]
+		else:
+			if (parsed_json["added_following"] not in user_info.following):
+				user_info.following += ";" + parsed_json["added_following"]
+		db.session.add(user_info)
+		db.session.commit()
+		return_dict = {'code': 200}
+		return_string = json.dumps(return_dict, sort_keys=True, indent=4, separators=(',',':'))
 		return return_string
 
 	def remove_user_following():
-	if 'user' in session.keys():
-		user = session['user']
-		parsed_json = request.get_json()
-		user_info = User_DBModel.query.filter_by(id = user["id"]).first()
-		if parsed_json['removed_following'] in user_info.following:
-			user_info.following.remove(parsed_json['removed_following'])
+	
 
+		parsed_json = request.get_json()
+		user_info = User_DBModel.query.filter_by(id = parsed_json["id"]).first()
+		if parsed_json['removed_following'] in user_info.following:
+			splitFollowing = parsed_json["removed_following"].split(';')
+			s = ""
+			for (var in splitFollowing):
+				if (parsed_json['removed_following'] not in var):
+					s += parsed_json['removed_following']
+			user_info.following.remove(parsed_json["removed_following"])
+
+
+		db.session.add(user_info)
 		db.session.commit()
 		return_dict = {'code': 200}
 		return_string = json.dumps(return_dict, sort_keys=True, indent=4, seperators=(',',': '))
 		return return_string
-	else:
-		dict_local = {'code': 31, 'message': "auth error"}
-		return_string = json.dumps(dict_local, sort_keys=True, indent=4, separators=(',', ': '))
-		return return_string
-
+	
 	def remove_user_follower():
-	if 'user' in session.keys():
+	
 		user = session['user']
 		parsed_json = request.get_json()
 		user_info = User_DBModel.query.filter_by(id = user["id"]).first()
@@ -193,10 +175,7 @@ class User():
 		return_dict = {'code': 200}
 		return_string = json.dumps(return_dict, sort_keys=True, indent=4, seperators=(',',': '))
 		return return_string
-	else:
-		dict_local = {'code': 31, 'message': "auth error"}
-		return_string = json.dumps(dict_local, sort_keys=True, indent=4, separators=(',', ': '))
-		return return_string
+	
 
 	@staticmethod
 	def update_user_info():
@@ -233,7 +212,6 @@ class User():
 				'id': user_info.id,
 				'email': user_info.email,
 				'name': user_info.name,
-				'tags': user_info.tags,
 				'following': user_info.following,
 				'followers': user_info.followers
 			}
