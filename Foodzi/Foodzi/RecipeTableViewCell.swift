43896dcb6928260626c06beab41ae8aab90cb02e
Bryan <bzhang57@gmail.com>
2018-03-03 00:45:21 -0500

    pushing view 1

---
+++ Foodzi/Foodzi/RecipeTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RecipeTableViewCell.swift
+//  Foodzi
+//
+//  Created by Bryan Zhang on 3/2/18.
+//  Copyright © 2018 Bryan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+    //Properties
+    @IBOutlet weak var recipeName: UILabel!
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
