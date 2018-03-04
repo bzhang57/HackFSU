e9d0b58a79b46b699d1f5f5453b9e2a22b426335
Kelly Chen <kell31ch@gmail.com>
2018-03-03 20:09:13 -0500

    seach view initialized

---
+++ Foodzi/Foodzi/SearchTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SearchTableViewCell.swift
+//  Foodzi
+//
+//  Created by Kelly Chen on 3/3/18.
+//  Copyright © 2018 Bryan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var trendingRecipe: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
